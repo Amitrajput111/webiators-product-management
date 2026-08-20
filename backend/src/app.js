0db754ebc218cc8d13b7e15180e4b444f7319ab3
Amit Rajput <amitrajput98267313@gmail.com>
2026-08-20 13:44:30 +0530

    feat: add global error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 
@@ -20,5 +21,7 @@ app.get('/api/health', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+
+app.use(errorMiddleware);
 
 module.exports = app;
